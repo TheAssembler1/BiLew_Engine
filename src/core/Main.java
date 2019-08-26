ae062f7f0b26a776242d0e05aa81dc4856afab13
TheAssembler1 <lewisnoah592@gmail.com>
2019-08-26 16:52:09 -0500

    This is the .2 version

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,90 +1,27 @@
 package core;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.Toolkit;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import game.User_Game;
 
-@SuppressWarnings("serial")
-public class Main extends JPanel{
-	
-	//lets main be visible
-	public static Main main;
-	
-	//sets up listeners and function call backs
-	public Main() {
-		KeyListener listener = new KeyListener() {
-			@Override
-			public void keyTyped(KeyEvent e) {
-				//Input.Key_Just_Pressed(e);
-			}
+import game.*; 
 
-			@Override
-			public void keyPressed(KeyEvent e) {
-				//Input.Key_Pressed(e);
-			}
+public class Main{
 
-			@Override
-			public void keyReleased(KeyEvent e) {
-				Input.Key_Released(e);
-			}
-		};
-		addKeyListener(listener);
-		setFocusable(true);
-	}
+	//main frame and panel
+	public static JFrame jframe;
+	public static JPanel jpanel;
 	
-	//overrides the paint method
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-		User_Game.Draw(g2d);
-		//syncrhonizes the graphics state
-		Toolkit.getDefaultToolkit().sync();
-	}
+	int x = 5;
+	int enemyx = 5;
 	
-	//entry point to the program
-	public static void main(String[] args) throws InterruptedException, IOException {
-		//stating version number
-		System.out.println("MESSAGE::VERSION .01");
+	public static void main(String[] args) throws InterruptedException {
+		//creating frame
+		Window window = new Window("BiLewEngine", Preferences.user_window_width, Preferences.user_window_height);
 		
-		//starting the program message
-		System.out.println("MESSAGE::STARTED PROGRAM");
-		
-		//loading the user sprites
-		User_Game.Sprites();
-		
-		//creating the frame
-		JFrame frame = new JFrame(User_Game.user_window_title);
-		main = new Main();
-		frame.add(main);
-		frame.setSize(User_Game.user_window_xsize, User_Game.user_window_ysize);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//created frame message
-		System.out.println("MESSAGE::SUCCESSFULLY CREATED FRAME");
-		
-		//creating the game loop variable
-		boolean game_run = true;
-		
-		//initializing game message
-		System.out.println("MESSAGE::INITIALIZING GAME LOOP");
-		
-		//this is the actual game loop
-		while (game_run) {
-			
-			//this is the user logic
+		//creating game loop
+		while (true) {
 			User_Game.Logic();
-			
-			main.repaint();
+			jframe.repaint();
 			Thread.sleep(10);
 		}
 	}
--- src/game/User_Game.java
+++ src/game/User_Game.java
@@ -1,37 +1,15 @@
 package game;
 
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+import java.awt.Graphics;
 
-public abstract class User_Game{
+public class User_Game {
 
-	//change these variables to your preference
-	public static int user_window_xsize = 500;
-	public static int user_window_ysize = 500;
-	public static String user_window_title = "Title";
-
-	
-	//default BiLew_Engine sprite
-	static BufferedImage blue;
-	
-	public static void Sprites() throws IOException {
-
-		//BiLew_Engine defualt sprite
-		blue = ImageIO.read(new File("src/game/Sprites/blue.png"));
-
-			
+	public static void Draw(Graphics g) {
+		
 	}
 	
-	//render things here
-	public static void Draw(Graphics2D g2d) {
-		g2d.drawImage(blue, 0, 0, null);
-	}
-	
-	//do logic here
 	public static void Logic() {
-
+		
 	}
+	
 }
